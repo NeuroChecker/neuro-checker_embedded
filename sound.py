a3d47c8c2ae79175868e7ddbc56b81262550446f
Timovanrijn <timo.vanrijn@student.fontys.nl>
2026-01-14 00:01:55 +0100

    updated sound.py to clases

--- sound.py
+++ sound.py
@@ -2,57 +2,54 @@ from machine import Pin, ADC
 from time import sleep
 import math
 
-# Initialization of ADC0
-analog_pin = ADC(1)
-
-# Initialization of GPIO21 as input
-digital_pin = Pin(21,Pin.IN, Pin.PULL_UP)
-
 # The set spanning for 1 Pa
 V0 = 0.00794
 
-# function to convert volts volts to db
-def volts_to_db(Volt):
-    if Volt <= 0:
-        # If V is to low, then math.log will crash, because log10(0) does not exist
+# function to convert volts to dB
+def volts_to_db(volt):
+    if volt <= 0:
+        # If V is too low, then math.log will crash, because log10(0) does not exist
         return float('-inf')
-    return 20 * math.log10(Volt / V0)
+    return 20 * math.log10(volt / V0)
 
-print("Microphone test")
-
-# Mesureing the base on startup
-print("Measuring base noise...")
-sum_volt = 0
-samples = 100
-
-for _ in range(samples):
-    raw_value = analog_pin.read_u16()
-    V = raw * 3.3 / 65536
-    sum_volt += V
-    sleep(0.005)
+class Microphone:
+    def __init__(self, analog_pin, digital_pin):
+        self.adc = ADC(analog_pin)  # pinnumber 1 but will come out of main.py
+        self.digital = Pin(digital_pin, Pin.IN, Pin.PULL_UP) #pinnumber will be in main.py
+        self.V_max = 3.3
+        self.samples = 100
+        self.baseline = None
     
-base = max(sum_volt / samples, 0.01)
-print("baseline", base, "V")
-
-
-# Endless loop for reading out the ADC
-while True:
-    raw_value = analog_pin.read_u16()
-    # Conversion from analog value to voltage
-    Volt = raw_value * 3.3 / 65536
+    def _raw_to_volt(self, raw):
+        return raw * self.V_max / 65536
     
-    level_db = volts_to_db(Volt)
+    def measure_baseline(self):
+        print("Measuring base noise...")
+        sum_volt = 0.0
+        
+        for _ in range(self.samples):
+            raw_value = self.adc.read_u16()
+            v = self._raw_to_volt(raw_value)
+            sum_volt += v
+            sleep(0.005)
+            
+        base = max(sum_volt / self.samples, 0.01)
+        self.baseline = base
+        print("Baseline:", base, "V")
+        return base
     
-    digital_pin_value = digital_pin.value()
+    def read(self):
+        raw = self.adc.read_u16()
+        volt = self._raw_to_volt(raw)
+        level_db = volts_to_db(volt)
+        digital_value = self.digital.value()
 
-    # Serial output of the analog value and the calculated voltage
-    print("Analog voltage value: " + str(Volt) + "Threshold value: " + str(level_db), end="")
+        return {
+            "raw": raw,
+            "volt": volt,
+            "db": level_db,
+            "digital": digital_value,
+            "baseline": self.baseline,
+        }
 
-    # Query whether the digital value has changed with serial output
-    if digital_pin_value == 1:
-        print("reached")
-    else:
-        print("not reached")
-    
-    sleep(2)
 
